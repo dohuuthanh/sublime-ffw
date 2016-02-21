1fd08470ecaa1a8ce958daced06f95e7faf7483b
Thanh Do <huuthanhcntt3a2@gmail.com>
2016-02-21 11:06:41 +0700

    Update file.

--- SublimeFFW.sh
+++ SublimeFFW.sh
@@ -3,26 +3,15 @@
 # Attempt OS detection to set path
 os=`uname`;
 
-# ST2 Packages directory
+# ST3 Packages directory
 if [ $os = 'Linux' ]
 then
-  st3DirUser=~/".config/sublime-text-3/Packages/User/"
+  # st3DirUser=~/".config/sublime-text-3/Packages/User/"
   st3Dir=~/".config/sublime-text-3/Packages/"
-else
-  # Assume OSX
-  st3DirUser=~/"Library/Application Support/Sublime Text 3/Packages/User/"
+  npmPath=/"/usr/local/lib/node_modules/"
 fi
 
-# Navigate to Packages DirectoryCKAGE
-cd "$st3DirUser";
-
-rm -rf "$st3DirUser"Preferences.sublime-settings;
-wget https://raw.githubusercontent.com/thanhdo1991/sublime-ffw/master/Preferences.sublime-settings
-
-sudo gem install scss_lint
-sudo npm install -g jshint
-
-cd '$st3Dir';
+cd "$st3Dir";
 
 # ExpandTabsOnSave
 if [ ! -d "ExpandTabsOnSave" ]; then
@@ -33,3 +22,20 @@ if [ ! -d "ExpandTabsOnSave" ]; then
   git pull origin master
   cd ..
 fi
+
+# Check scss_lint exist.
+if [ ! "gem list -i scss_lint" ]; then
+ sudo gem install scss_lint;
+fi
+
+cd "$npmPath"
+
+# jshint
+if [ ! -d "jshint" ]; then
+ sudo npm install -g jshint;
+fi
+
+# Navigate to Packages User
+cd "$st3Dir"User;
+
+wget -N https://raw.githubusercontent.com/thanhdo1991/sublime-ffw/master/Preferences.sublime-settings
