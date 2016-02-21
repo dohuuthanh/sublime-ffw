0766fc6c0cd3e1a8dbc1b233f089f841e6f3858c
Thanh Do <huuthanhcntt3a2@gmail.com>
2016-02-21 10:31:13 +0700

    Update Preference settings.

--- SublimeFFW.sh
+++ SublimeFFW.sh
@@ -7,6 +7,7 @@ os=`uname`;
 if [ $os = 'Linux' ]
 then
   st3DirUser=~/".config/sublime-text-3/Packages/User/"
+  st3Dir=~/".config/sublime-text-3/Packages/"
 else
   # Assume OSX
   st3DirUser=~/"Library/Application Support/Sublime Text 3/Packages/User/"
@@ -21,4 +22,14 @@ wget https://raw.githubusercontent.com/thanhdo1991/sublime-ffw/master/Preference
 sudo gem install scss_lint
 sudo npm install -g jshint
 
+cd '$st3Dir';
 
+# ExpandTabsOnSave
+if [ ! -d "ExpandTabsOnSave" ]; then
+ git clone https://github.com/thanhdo1991/ExpandTabsOnSave.git;
+ else
+  echo "Updating plugin ExpandTabsOnSave";
+  cd "ExpandTabsOnSave"
+  git pull origin master
+  cd ..
+fi
